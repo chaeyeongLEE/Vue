496df1cff5dfc0fcbc45b3d4a64f5d869b6d9d24
chaeyeongLEE <dh503401@naver.com>
2023-05-15 23:52:48 +0900

    feat: modal

--- 230511/src/App.vue
+++ 230511/src/App.vue
@@ -1,4 +1,14 @@
 <template>
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="모달창열렸니=false">닫기</button>
+    </div>
+  </div>
+
+
+
 
   <div class="menu">
     <!-- <a>Home</a>
@@ -12,16 +22,24 @@
   <p> {{ price1 }} 만원</p>
  </div> -->
   <div>
-  <h4> {{products[0]}}</h4>
-  <p> {{price1}}만원 </p>
+  <img :src="원룸들[0].image" class="room-img"/>
+  <h4> {{원룸들[0].title}}</h4>
+  <p> {{원룸들[0].price}}원 </p>
  <button @click="신고수[0]++">허위매물신고</button> <span>신고수 : {{신고수[0]}} </span>
   </div>
+
+
+  <div v-for="(방들, i) in 원룸들" :key="i">{{방들[i]}}</div>
+
+
   <div>
+   <img src="./assets/room1.jpg" class="room-img"/> 
   <h4> {{products[1]}}</h4>
   <p> {{price2}}만원 </p>
   <button @click="신고수[1]++">허위매물신고</button> <span>신고수 : {{신고수[1]}} </span>
   </div>
    <div>
+  <img src="./assets/room2.jpg" class="room-img"/>
   <h4>{{products[2]}}</h4>
   <p> {{price3}}만원 </p>
   <button @click="신고수[2]++">허위매물신고</button> <span>신고수 : {{신고수[2]}} </span>
@@ -32,6 +50,7 @@
 // html에서는 아래와 같이 코드를 사용했었다.
 //document.getElementById().innerHTML = ??
 
+import data from './assets/oneroom.js';
 export default {
   name: 'App',
   data() {
@@ -42,6 +61,9 @@ export default {
       price1 : 60, 
       price2 : 70,
       price3 : 40,
+      모달창열렸니 : false,
+      원룸들: data,
+      //[{},{}.{}] 이러한 형태로 원룸들이 있음
       신고수 : [0,0,0],
       메뉴들 : ['home','shop','about'],
       products : ['역삼동원룸','천호동원룸','마포구 원룸']
@@ -60,6 +82,26 @@ export default {
 </script>
 
 <style>
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
+.room-img{
+  width: 100%;
+  margin-top: 40px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -78,6 +120,8 @@ export default {
   color: aliceblue;
   padding: 10px;
 }
+
+
 
 
 </style>
