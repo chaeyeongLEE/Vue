ecc10b3f0fc2e6cc4f0246cc68bda50b480c64cd
chaeyeongLEE <dh503401@naver.com>
2023-05-31 17:59:28 +0900

    feat: stagram and props

---
+++ vuestagram/src/App.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
+  </div>
+
+<NewContainer :post="post" />
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
+</template>
+
+<script>
+import NewContainer from "@/components/NewContainer.vue";
+import insta from "../src/assets/insta";
+export default {
+  name: 'App',
+  data(){
+    return {
+      post : insta
+      // insta라는 이름으로 불러온 데이터를 post라는 이름으로 저장
+    }
+  },
+  components: {
+    NewContainer : NewContainer
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
+  text-align: center;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+.inputfile {
+  display: none;
+}
+.input-plus {
+  cursor: pointer;
+}
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
+  margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
+}
+</style>
---
+++ vuestagram/src/components/NewContainer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <NewPost :post="post[i]" v-for="(a, i) in post" :key="i" />
+<!--<NewPost :post="a" v-for="(a, i) in post" :key="i" /> 라고도 작성할 수 있다.-->
+
+  <!--  아래는 하드코딩 , 위는 반복문 이용
+    <NewPost :post="post[0]" />
+    <NewPost :post="post[1]" />
+    <NewPost :post="post[2]" /> -->
+
+  </div>
+</template>
+<script>
+import NewPost from "./NewPost.vue"
+export default {
+  name: 'NewContainer',
+  props: {
+      post : Array,
+    }
+  ,
+  components: {NewPost : NewPost},
+}
+</script>
+<style></style>
